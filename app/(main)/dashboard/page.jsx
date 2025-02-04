b844a4759b7c0777505ee1de8f5c6aaf0b4282ae
Олег bc2024-5 <olegchaplia2006@gmail.com>
2025-02-04 15:03:19 +0200

    Created models for the database, added routing and a dashboard page

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DashboardPage = () => {
+  return (
+    <div className="px-5 ">
+      {/* Budget progress */}
+
+      {/* Overview */}
+
+      {/* Accouts grid */}
+    </div>
+  );
+};
+
+export default DashboardPage;
